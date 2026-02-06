2ebcd2c5d52841dba964368b19ca9b09e80e48cc
ElKnoxville <frankc.r121@gmail.com>
2026-02-06 14:29:56 -0500

    Update co_sheets.py

--- lib_resources/co_sheets.py
+++ lib_resources/co_sheets.py
@@ -4,7 +4,6 @@ import os
 import sys
 
 def conectar_sheet():
-    """Conecta a Google Sheets y devuelve el objeto de la hoja."""
     try:
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         ruta_json = os.path.join(BASE_DIR, "..", "credentials", "credentials.json")
@@ -17,9 +16,9 @@ def conectar_sheet():
         creds = ServiceAccountCredentials.from_json_keyfile_name(ruta_json, scope)
         client = gspread.authorize(creds)
 
-        # Conexión específica a tu hoja
         sheet = client.open("rimac 24-25 ULTIMO").worksheet("2024")
         return sheet
     except Exception as e:
         print(f"Error conectando a Sheets: {e}")
+
         return None
